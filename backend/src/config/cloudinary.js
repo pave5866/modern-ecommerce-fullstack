f458fcf3f755938de68640b18c727af42000e3ea
Kadir ERDEM <49350752+pave5866@users.noreply.github.com>
2025-03-16 14:53:35 +0300

    Cloudinary yapılandırması düzeltildi

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -1,9 +1,32 @@
 const cloudinary = require('cloudinary').v2;
+const logger = require('../utils/logger');
 
+// Cloudinary yapılandırması
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-module.exports = cloudinary; 
+// Yapılandırma kontrolü
+try {
+  logger.info('Cloudinary yapılandırması kontrol ediliyor...');
+  
+  if (!process.env.CLOUDINARY_CLOUD_NAME || 
+      !process.env.CLOUDINARY_API_KEY || 
+      !process.env.CLOUDINARY_API_SECRET) {
+    logger.error('Cloudinary yapılandırma bilgileri eksik!', {
+      cloud_name: !!process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: !!process.env.CLOUDINARY_API_KEY,
+      api_secret: !!process.env.CLOUDINARY_API_SECRET
+    });
+  } else {
+    logger.info('Cloudinary yapılandırması başarılı', {
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME
+    });
+  }
+} catch (error) {
+  logger.error('Cloudinary yapılandırma hatası:', error.message);
+}
+
+module.exports = cloudinary;
