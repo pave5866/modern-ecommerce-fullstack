d165471cd6c6137d7589e1125c3c42eab2f71936
Kadir ERDEM <49350752+pave5866@users.noreply.github.com>
2025-03-17 13:52:17 +0300

    HelmetProvider eklendi

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,13 @@ import { router } from './router'
 import './index.css'
 import './i18n' // i18next başlatma
 import { Toaster } from 'react-hot-toast'
+import { HelmetProvider } from 'react-helmet-async'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <>
+    <HelmetProvider>
       <Toaster position="top-center" />
       <RouterProvider router={router} />
-    </>
+    </HelmetProvider>
   </React.StrictMode>,
 )
