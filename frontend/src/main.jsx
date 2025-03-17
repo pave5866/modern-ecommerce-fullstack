4cf67fc013cbe9bfdd0ca0d8328562b46d63d4e4
Kadir ERDEM <49350752+pave5866@users.noreply.github.com>
2025-03-17 13:43:25 +0300

    Frontend uygulaması düzeltildi: RouterProvider kullanımına geçildi

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router'
 import './index.css'
 import './i18n' // i18next başlatma
+import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <>
+      <Toaster position="top-center" />
+      <RouterProvider router={router} />
+    </>
   </React.StrictMode>,
 )
