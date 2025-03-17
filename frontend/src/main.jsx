b095dc2dff90b778ceb23c998254e255ca1b7b5f
Kadir ERDEM <49350752+pave5866@users.noreply.github.com>
2025-03-17 13:56:46 +0300

    main.jsx güncellendi

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './router'
-import './index.css'
-import './i18n' // i18next başlatma
-import { Toaster } from 'react-hot-toast'
 import { HelmetProvider } from 'react-helmet-async'
+import router from './routes'
+import './index.css'
+import './i18n'
+import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
-      <Toaster position="top-center" />
       <RouterProvider router={router} />
+      <Toaster 
+        position="top-right"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+        }}
+      />
     </HelmetProvider>
   </React.StrictMode>,
 )
