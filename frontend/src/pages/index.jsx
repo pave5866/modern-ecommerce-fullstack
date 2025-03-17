63151b64d993bc14dc066727433939db07d69666
Kadir ERDEM <49350752+pave5866@users.noreply.github.com>
2025-03-17 13:43:35 +0300

    Sayfalar için merkezi export dosyası eklendi

---
+++ frontend/src/pages/index.jsx
@@ -0,0 +1,18 @@
+// Tüm sayfalar için merkezi export
+import Home from './Home'
+import Products from './Products'
+import ProductDetail from './ProductDetail'
+import Cart from './Cart'
+import Orders from './Orders'
+import Profile from './Profile'
+import NotFound from './NotFound'
+
+export {
+  Home,
+  Products,
+  ProductDetail,
+  Cart,
+  Orders,
+  Profile,
+  NotFound
+}
